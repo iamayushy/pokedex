c10b0aafb3584aaedd6c38904065d9afbebbe8bf
iamayushy <ayushhuns@gmail.com>
2022-08-30 12:48:22 +0530

    first java

---
+++ One/One.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class One {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        int x = 34;
+        int y = 23;
+        int sum = x + y;
+        System.out.println(age);
+       
+    }
+
+
+}
+
